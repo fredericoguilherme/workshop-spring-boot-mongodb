b06900e813219b89c58609332c95f18e6f62700c
fredericoguilherme <guilherme.github@gmail.com>
2022-02-18 17:17:54 +0000

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/fredericoguilherme/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.fredericoguilherme.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fredericoguilherme.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+
+}
