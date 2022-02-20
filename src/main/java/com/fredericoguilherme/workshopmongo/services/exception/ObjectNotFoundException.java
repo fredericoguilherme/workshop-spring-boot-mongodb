e616950f061f1fb0f9dfa192fe47aeaf8d2a4be3
fredericoguilherme <guilherme.github@gmail.com>
2022-02-20 17:19:14 +0000

    Obtendo um usuário por id

---
+++ src/main/java/com/fredericoguilherme/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.fredericoguilherme.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+}
